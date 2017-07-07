911f6fb76aa52b2d316d99119cf1f2ad69eeccb8
Alex Schouls <alex.schouls@valtech.nl>
2017-07-07 14:37:17 +0200

    Set up solution and initial projects

---
+++ API/VRIS.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VRIS.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ API/VRIS.Services/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VRIS.Services
+{
+    public class Class1
+    {
+    }
+}
