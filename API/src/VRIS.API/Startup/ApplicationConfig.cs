b2fedbcc2f9cfba8efa8e939a74b475b972cf088
Marvin Brouwer <marvin.brouwer@valtech.nl>
2017-07-07 16:22:39 +0200

    Fixed swagger

--- API/src/VRIS.API/Startup/ApplicationConfig.cs
+++ API/src/VRIS.API/Startup/ApplicationConfig.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Valtech.Labs.ValtechPointer.Portal.Application
+namespace VRIS.API.Startup
 {
     /// <summary>
     /// Application Config for the VRIS Api
--- API/src/VRIS.API/Startup/VrisApp.cs
+++ API/src/VRIS.API/Startup/VrisApp.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Valtech.Labs.ValtechPointer.Portal.Application;
 
 namespace VRIS.API.Startup
 {
