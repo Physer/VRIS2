c0e7b7e91e1618816928f82aaeeb6e308aaac28c
Alex Schouls <alex.schouls@valtech.nl>
2017-07-21 15:57:31 +0200

    Added an auth controller for MS Open Graph

---
+++ API/src/VRIS.API/Controllers/AuthorizationController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace VRIS.API.Controllers
+{
+    [Route("api/authorization")]
+    public class AuthorizationController
+    {
+        [Route("/api/authorization/get")]
+        [HttpGet]
+        public void Get(string code)
+        {
+            var x = code;
+        }
+    }
+}
