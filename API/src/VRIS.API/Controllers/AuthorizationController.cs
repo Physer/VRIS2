4a794900b20eecec98f15b6a6bde8586f6f6a2f0
Alex Schouls <alex.schouls@valtech.nl>
2017-07-26 09:43:48 +0200

    Created quick stub for retrieving auth code to get a token with

--- API/src/VRIS.API/Controllers/AuthorizationController.cs
+++ API/src/VRIS.API/Controllers/AuthorizationController.cs
@@ -7,9 +7,14 @@ namespace VRIS.API.Controllers
     {
         [Route("/api/authorization/get")]
         [HttpGet]
-        public void Get(string code)
-        {
-            var x = code;
-        }
+        public string Get(string code) => code;
+
+        [HttpGet]
+        [Route("/api/authorization/login")]
+        public ActionResult Login() => new RedirectResult("https://login.microsoftonline.com/common/oauth2/v2.0/authorize?client_id=9d02646e-b39f-4e26-b330-e3434a51585c&response_type=code&redirect_uri=http://localhost:50796/api/authorization/get&response_mode=query&scope=user.readbasic.all calendars.readwrite.shared");
+
+        [HttpPost]
+        [Route("/api/authorization/token")]
+        public string Token(string code) => string.Empty;
     }
 }
