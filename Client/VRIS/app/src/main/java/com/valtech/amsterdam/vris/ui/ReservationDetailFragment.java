1268cad07418b00b5b21e96c06d9f3716ed8d660
Marvin Brouwer <marvin.brouwer@valtech.nl>
2017-07-28 13:00:08 +0200

    Using databinding in reservation view

--- Client/VRIS/app/src/main/java/com/valtech/amsterdam/vris/ui/ReservationDetailFragment.java
+++ Client/VRIS/app/src/main/java/com/valtech/amsterdam/vris/ui/ReservationDetailFragment.java
@@ -4,18 +4,18 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.GridView;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import com.valtech.amsterdam.vris.VrisAppContext;
 import com.valtech.amsterdam.vris.R;
+import com.valtech.amsterdam.vris.databinding.TimeslotDetailReservationBinding;
 import com.valtech.amsterdam.vris.model.Reservation;
+import com.valtech.amsterdam.vris.model.Room;
 
 import org.joda.time.DateTime;
 
@@ -26,7 +26,10 @@ import org.joda.time.DateTime;
  */
 public class ReservationDetailFragment extends BaseTimeSlotFragment {
 
-    private View rootView;
+    private TimeslotDetailReservationBinding mTimeslotDetailReservationBinding;
+    private BroadcastReceiver mBroadcastReceiver;
+    public final String fDateFormat = "dd-MM-yyyy";
+    public final String fTimeFormat = "HH:mm";
 
     /**
      * Mandatory empty constructor for the fragment manager to instantiate the
@@ -42,71 +45,42 @@ public class ReservationDetailFragment extends BaseTimeSlotFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-            Bundle savedInstanceState) {
-        rootView = inflater.inflate(R.layout.timeslot_detail_reservation, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        mTimeslotDetailReservationBinding = DataBindingUtil.inflate( inflater, R.layout.timeslot_detail_reservation, container, false);
 
-        SetDefaultTextViews();
         if (mTimeSlot != null) {
-            SetReservationTextViews();
+            Reservation reservationItem = (Reservation)mTimeSlot;
+            mTimeslotDetailReservationBinding.setFragment(this);
+            mTimeslotDetailReservationBinding.setRoom(new Room(2, "Some room"));
+            mTimeslotDetailReservationBinding.setDateTime(DateTime.now().toLocalDateTime());
+            mTimeslotDetailReservationBinding.setReservation(reservationItem);
+            mTimeslotDetailReservationBinding.reservationOrganizer.setOrganizer(reservationItem.getOrganizer());
+            mTimeslotDetailReservationBinding.attendeeGrid.setAdapter(new AttendeeGridAdapter(this.getContext(), reservationItem));
+
+            mBroadcastReceiver = new BroadcastReceiver() {
+                @Override
+                public void onReceive(Context ctx, Intent intent) {
+                    if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
+                        mTimeslotDetailReservationBinding.setDateTime(DateTime.now().toLocalDateTime());
+                }
+            };
+
+            this.getContext().registerReceiver(mBroadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
         }
-
-        return rootView;
+        return mTimeslotDetailReservationBinding.getRoot();
     }
 
-    private void SetReservationTextViews() {
-        Reservation reservationItem = (Reservation)mTimeSlot;
-        TextView reservationTitleElement = (TextView) rootView.findViewById(R.id.reservation_title);
-        TextView reservationTimeElement = (TextView) rootView.findViewById(R.id.reservation_time);
-        TextView organizerNameElement = (TextView) rootView.findViewById(R.id.organizer_name);
-        TextView organizerEmailElement = (TextView) rootView.findViewById(R.id.organizer_email);
-        // todo lazy load
-        ImageView organizerIconElement = (ImageView) rootView.findViewById(R.id.organizer_image);
-        GridView attendees = (GridView) rootView.findViewById(R.id.attendee_grid);
-
+    public String formatReservationTimes(Reservation reservation){
         StringBuilder reservationTime = new StringBuilder();
-        reservationTime.append(reservationItem.getStartDate().toString("HH:mm"));
+        reservationTime.append(reservation.getStartDate().toString("HH:mm"));
         reservationTime.append(" - ");
-        reservationTime.append(reservationItem.getEndDate().toString("HH:mm"));
-
-        reservationTitleElement.setText(reservationItem.getTitle());
-        reservationTimeElement.setText(reservationTime.toString());
-        organizerNameElement.setText(reservationItem.getOrganizer().getName());
-        organizerEmailElement.setText(reservationItem.getOrganizer().getEmail());
-
-        attendees.setAdapter(new AttendeeGridAdapter(this.getContext(), reservationItem));
-    }
-
-    private void SetDefaultTextViews() {
-        // todo register on login from all rooms in api and print here
-        TextView currentRoomElement = (TextView) rootView.findViewById(R.id.current_room);
-        TextView currentDateElement = (TextView) rootView.findViewById(R.id.current_date);
-        TextView currentTimeElement = (TextView) rootView.findViewById(R.id.current_time);
-
-        currentDateElement.setText(DateTime.now().toLocalDateTime().toString("dd-MM-yyyy"));
-        currentTimeElement.setText(DateTime.now().toLocalDateTime().toString("HH:mm"));
-    }
-
-    BroadcastReceiver _broadcastReceiver;
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        _broadcastReceiver = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context ctx, Intent intent) {
-                if (intent.getAction().compareTo(Intent.ACTION_TIME_TICK) == 0)
-                    SetDefaultTextViews();
-            }
-        };
-
-        this.getContext().registerReceiver(_broadcastReceiver, new IntentFilter(Intent.ACTION_TIME_TICK));
+        reservationTime.append(reservation.getEndDate().toString("HH:mm"));
+        return reservationTime.toString();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        if (_broadcastReceiver != null)
-            this.getContext().unregisterReceiver(_broadcastReceiver);
+        if (mBroadcastReceiver != null) this.getContext().unregisterReceiver(mBroadcastReceiver);
     }
 }
