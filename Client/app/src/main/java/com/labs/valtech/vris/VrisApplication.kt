65525a256141806958d2e9bdb918c98c90571942
Marvin Brouwer <marvin.brouwer@valtech.nl>
2017-12-22 14:24:47 +0100

    Added state to app

--- Client/app/src/main/java/com/labs/valtech/vris/VrisApplication.kt
+++ Client/app/src/main/java/com/labs/valtech/vris/VrisApplication.kt
@@ -7,8 +7,14 @@ import android.app.Application
 import com.github.salomonbrys.kodein.*
 import com.labs.valtech.vris.repositories.settings.ISettingRepository
 import com.labs.valtech.vris.repositories.settings.SettingRepository
+import com.orhanobut.hawk.Hawk
 
 class VrisApplication : Application(), KodeinAware {
+
+    override fun onCreate() {
+        super.onCreate()
+        Hawk.init(this.applicationContext).build();
+    }
 
     override val kodein by Kodein.lazy {
 
--- Client/app/src/main/java/com/labs/valtech/vris/repositories/settings/SettingRepository.kt
+++ Client/app/src/main/java/com/labs/valtech/vris/repositories/settings/SettingRepository.kt
@@ -1,15 +1,20 @@
 package com.labs.valtech.vris.repositories.settings
 
+import com.orhanobut.hawk.Hawk
+
 /**
  * Created by marvin.brouwer on 19-12-2017.
  */
 
 class SettingRepository: ISettingRepository {
 
-    private var _roomId: String? = null;
+    private var _roomId: String? = Hawk.get("roomId", null)
 
     override var RoomId: String?
         get() = this._roomId
-        set(value: String?) { this._roomId = value!! }
+        set(value: String?) {
+            this._roomId = value!!
+            Hawk.put("roomId", value)
+        }
 
 }
